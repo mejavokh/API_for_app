62d262583441e81d1d3b9cf518f24f34dddbcf52
mejavokh <tavhidovjavohir@gmail.com>
2024-10-14 14:35:55 +0500

    debug urls

--- main/urls.py
+++ main/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('product/<int:id>/', ProductDetailView.as_view(), name='product-detail'),
 ]
 
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
